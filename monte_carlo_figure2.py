4cd702dff394f4410f0479b86edbc67503d46196
stephtdouglas <stephanietdouglas@gmail.com>
2016-05-26 10:07:06 -0400

    use where instead of looping over every injection

--- monte_carlo_figure2.py
+++ monte_carlo_figure2.py
@@ -15,32 +15,32 @@ from PyAstronomy.funcFit import SinusFit1d
 import numpy as np
 from scipy.optimize import leastsq
 
-import matplotlib.patches as mpatches
+import matplotlib.patches as mpatches  
 import astropy.io.ascii as at
-
-logging.basicConfig(level=logging.DEBUG)
+      
+logging.basicConfig(level=logging.DEBUG)  
 
 def monte_carlo_figure2(files):
-
+    
     for filename in files:
         logging.info(filename)
         if filename.endswith(".csv"):
-
+            
             tbl = at.read(str(root)+'/'+str(file))
             unique = tbl["unique"]
             injected_period = tbl["injected_period"]
             new_period = tbl["new_period"]
             max_power = tbl["max_power"]
-
-            for i in range(0, len(unique)):
-                if max_power[i] > 0.2 and max_power[i] < 0.3:
-                    plt.plot(injected_period[i], new_period[i], marker='D', c='lightcoral', markersize=0.5, fillstyle='none')
-                if unique[i] == 1:
-                    if max_power[i] > 0.3 and max_power[i] < 0.6:
-                        plt.plot(injected_period[i], new_period[i], marker='D', c='khaki', markersize=0.5, fillstyle='none')
-                    if max_power[i] > 0.6:
-                        plt.plot(injected_period[i], new_period[i], marker='D', c='black', markersize=0.5, fillstyle='none')
-
+            
+            a = np.where((max_power > 0.2) & (max_power < 0.3))[0]
+            plt.scatter(injected_period[a], new_period[a], marker='D', color='lightcoral', s=0.5)
+            
+            b = np.where((unique == 1) & (max_power > 0.3) & (max_power < 0.6))[0]
+            plt.scatter(injected_period[b], new_period[b], marker='D', color='khaki', s=0.5)
+            
+            c = np.where((unique == 1) & (max_power > 0.6))[0]
+            plt.scatter(injected_period[c], new_period[c], marker='D', color='black', s=0.5)        
+                                  
     ax = plt.gca()
     ax.set_xscale('log')
     ax.set_yscale('log')
@@ -51,13 +51,13 @@ def monte_carlo_figure2(files):
     red_patch = mpatches.Patch(color='lightcoral', label='0.2 < power < 0.3')
     yellow_patch = mpatches.Patch(color='khaki', label='unique = 1 & 0.3 < power < 0.6')
     black_patch = mpatches.Patch(color='black', label='unique = 1 & power > 0.6')
-    plt.legend(handles=[black_patch, yellow_patch, red_patch])
-    plt.savefig("Figure2.eps")
-
-
-
+    plt.legend(handles=[black_patch, yellow_patch, red_patch], loc=2)    
+    plt.savefig("/Users/Leo/Desktop/Figure 2.eps")
+    
+    
+    
 if __name__=="__main__":
-
+    
     if len(sys.argv)==1:
         print("Please provide a list of injection files")
     else:
