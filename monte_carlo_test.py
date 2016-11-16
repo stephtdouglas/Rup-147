30051f816da09b40fa7f494ce3be1f071c8ca43e
stephtdouglas <stephanietdouglas@gmail.com>
2016-11-16 16:39:32 -0500

    streamlined injections function and input light curve as an argument, not filename

--- monte_carlo_test.py
+++ monte_carlo_test.py
@@ -1,11 +1,10 @@
 import sys, os
 import logging
 from os import walk
-from numpy import loadtxt, zeros, random, mean, std, linspace, argmax, percentile
 
 import matplotlib.pyplot as plt
 
-from astroML.time_series import lomb_scargle, lomb_scargle_bootstrap
+from astroML.time_series import lomb_scargle_fast
 
 from math import log10, sin, pi
 
@@ -90,87 +89,87 @@ def test_pgram(periods, powers, threshold, n_aliases, alias_with):
     # Return best_period, best_power, aliases, is_clean
     return is_clean
 
-def monte_carlo_injections(files):
+def monte_carlo_injections(time, mag, error, output_filename,
+                           N_inject=1e5, short_P=0.1, long_P=100):
+    """
+    Inject the input light curve with randomly generated sine curves and
+    measure the accuracy of the returned period.
 
-    N = 100000
-    short_P = 0.1
-    long_P = 62.0
+    Inputs:
+    time, mag, error: array_like.
+         light curve to be analyzed.
+         Assumes all elements are nonzero and cleaned
+
+    output_filename: string
+
+    N_inject: integer (default=10^5)
+         Number of periods to injected
+
+    short_P, long_P: float (defaults = 0.1, 100)
+         min and max periods to inject, in days
+
+    """
+
     pd = np.linspace(short_P, long_P, N)
-    f = (2.0*pi)/pd
+    f = 2.0*np.pi/pd
     # 2pi/f = 2pi/(2pi/pd) = pd
     p0 = np.log10(pd)
 
 
-    for fcount,filename in enumerate(files):
-        logging.info(filename)
-        if filename.endswith("sysrem.txt"):
+    # Set up periods to be injected
+    n_periods = 500
+    n_amplitudes = 5
+    periods = 31.5 * random.random_sample(n_periods) + 0.1
+    injected_period = np.zeros((n_amplitudes,n_periods))
+    for i in range(n_amplitudes):
+        injected_period[i] = periods
 
-            injected_period = []
-            newperiod = []
-            max_power = []
-            unique = []
+    # set up amplitudes to be used
+    std01 = np.std(mag[np.isfinite(mag)==True])
+    amplitude = np.array([std01*0.3, std01*0.6, std01*0.9,
+                          std01*1.2, std01*1.5])
+    injected_amplitudes = np.zeros((n_amplitudes,n_periods))
+    for j in range(n_periods):
+        injected_amplitudes[:,j] = amplitude
 
+    # arrays to track the results of injections
+    new_period = np.ones_like(injected_period)*-9999.0
+    max_power = np.copy(new_period)
+    unique = np.zeros((n_amplitudes,n_periods),int)
 
-            n_periods = 500
-            periods = 31.5 * random.random_sample(n_periods) + 0.1
-            new = np.array(periods).tolist()
+    all_count = 0
+    for i in range(n_amplitudes):
+        for j in range(n_periods):
+            logging.info("%d %0.3f %d %0.3f", i, amplitude[i], j, periods[j])
+            logging.debug("%d %0.3f", all_count, injected_period[all_count])
+            sinusoid = amplitude[i] * np.sin(2*pi*(time-time[0])/periods[j])
+            newmag = mag + sinusoid
 
-            injected_period = np.array(injected_period + new * 5)
+            pgram_g = lomb_scargle_fast(time, newmag, error, f,
+                                        generalized=True)
 
-            file00 = open(filename, 'r')
-            time, mag, error, ra, dec, flag = loadtxt(file00, dtype='float',delimiter=' ',
-                                                      usecols=(0,1,2,3,4,7), unpack=True)
-            inputLC00 = zip(time[0:], mag[0:], error[0:], ra[0:], dec[0:], flag[0:])
-            inputLC01 = [[time,mag,error,ra,dec,flag] for (time,mag,error,ra,dec,flag) in inputLC00 if \
-			((error != 0.) and (ra != 0.) and (dec != 0.) and (flag == 0.))]
-#	    std01 = std([item[1] for item in inputLC01])
-#	    amplitude = np.array([std01*0.3, std01*0.6, std01*0.9, std01*1.2, std01*1.5])
-	    time = np.array([item[0] for item in inputLC01])
-	    mag = np.array([item[1] for item in inputLC01])
-	    error = np.array([item[2] for item in inputLC01])
-	    std01 = np.std(mag[np.isfinite(mag)==True])
-	    amplitude = np.array([std01*0.3, std01*0.6, std01*0.9, std01*1.2, std01*1.5])
-            n_amplitudes = len(amplitude)
+            # Find the peak of the new periodogram
+            max_loc = np.argmax(pgram_g)
+            newperiod[i,j] = pd[max_loc]
+            max_power[i,j] = pgram_g[max_loc]
 
-            injected_amplitudes = np.zeros(n_periods*5)
+            is_clean = test_pgram(pd, pgram_g, threshold=0.6, n_aliases=3)
 
-            all_count = 0
-	    for i in range(0, len(amplitude)):
-	        for j in range(0, n_periods):
-                    logging.info("%d %0.3f %d %0.3f",i,amplitude[i],j,periods[j])
-                    injected_amplitudes[all_count] = amplitude[i]
-                    logging.debug("%d %0.3f",all_count,injected_period[all_count])
-#	            sinusoid = [amplitude[i] * sin(2*pi*(x-time[0])/periods[j]) for x in time]
-#	            newmag = [a+b for a,b in zip(mag, sinusoid)]
-                    #print(type(time),np.shape(time),time.dtype)
-	            sinusoid = amplitude[i] * np.sin(2*pi*(time-time[0])/periods[j])
-	            newmag = mag+sinusoid
+            if is_clean is True:
+                unique[i,j] = 1
+            else:
+                unique[i,j] = 0
 
-                    pgram_g = lomb_scargle(time, newmag, error, f, generalized = True)
+    data = {"injected_amplitude": injected_amplitudes.flatten(),
+            "injected_period": injected_period.flatten(),
+            "new_period": newperiod.flatten(),
+            "max_power": max_power.flatten(),
+            "unique": unique.flatten()}
 
-                    # Find the peak of the new periodogram
-                    max_loc = np.argmax(pgram_g)
-                    newperiod = newperiod + [pd[max_loc]]
-                    max_power = max_power + [pgram_g[max_loc]]
+    names = ["injected_amplitude", "injected_period", "new_period",
+             "max_power", "unique"]
 
-                    is_clean = test_pgram(pd, pgram_g, threshold=0.6, n_aliases=3, alias_with=1.0)
-
-                    if is_clean is True:
-                        unique = unique + [1]
-                    else:
-                        unique = unique + [0]
-
-                    all_count += 1
-
-            data = {"injected_amplitude": injected_amplitudes,
-                    "injected_period": injected_period,
-                    "new_period": newperiod,
-                    "max_power": max_power,
-                    "unique": unique}
-
-            names = ["injected_amplitude","injected_period", "new_period", "max_power", "unique"]
-
-            at.write(data,str(filename)[:-4]+".injections.csv",names=names,delimiter=",")
+    at.write(data, output_filename, names=names, delimiter=",")
 
 #            if fcount>=0:
 #                break
@@ -178,26 +177,27 @@ def monte_carlo_injections(files):
 
 if __name__=="__main__":
 
-    if len(sys.argv)==1:
-        print("Please provide a list of light curve files")
-    else:
-        listfile = at.read(sys.argv[1])
-        file_list = listfile["filename"]
-
-    # Break down the data set into subsets for parallel processing
-    arrayid = int(os.getenv("PBS_ARRAYID",0))
-
-    arr_stride = 8
-    mini = (arrayid - 1) * arr_stride
-    maxi = min(mini + arr_stride, len(file_list))
-    if arrayid==0:
-        mini = 0
-        maxi = len(file_list)
-
-    print("Array, min(i), max(i)")
-    print(arrayid, mini, maxi)
-
-    sub_list = file_list[mini:maxi]
-    print(sub_list)
-
-    monte_carlo_injections(sub_list)
+    pass
+    # if len(sys.argv)==1:
+    #     print("Please provide a list of light curve files")
+    # else:
+    #     listfile = at.read(sys.argv[1])
+    #     file_list = listfile["filename"]
+    #
+    # # Break down the data set into subsets for parallel processing
+    # arrayid = int(os.getenv("PBS_ARRAYID",0))
+    #
+    # arr_stride = 8
+    # mini = (arrayid - 1) * arr_stride
+    # maxi = min(mini + arr_stride, len(file_list))
+    # if arrayid==0:
+    #     mini = 0
+    #     maxi = len(file_list)
+    #
+    # print("Array, min(i), max(i)")
+    # print(arrayid, mini, maxi)
+    #
+    # sub_list = file_list[mini:maxi]
+    # print(sub_list)
+    #
+    # monte_carlo_injections(sub_list)
